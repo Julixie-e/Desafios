97d2cc2906fd3dce88548d4fbc655fec6c916f76
Julixie-e <xiejuli01@gmail.com>
2024-05-05 19:26:03 -0300

    termine el desafío :)

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let nombreProducto = "Bombones de Chocolate"
+let precioUnitario =  800
+let CantidadDeseada = prompt (" Ingrese la cantidad de bombones de chocolate que desea comprar")
+
+let CostoFinal = precioUnitario * CantidadDeseada
+
+alert(CostoFinal); 
